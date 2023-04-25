dd9cd2ebbb03ccade5b5526becc793f293850837
Hassen <hassenbensaad55@gmail.com>
2023-04-26 00:08:12 +0200

    Initial Commit

---
+++ src/test/java/com/example/CinemaServiceApplication/CinemaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaServiceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
